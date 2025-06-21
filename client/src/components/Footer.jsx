b26022c7bd1eb21bd45379e25a63df6d7576ddd6
Pranavsai058 <pranavsaikapilavai@gmail.com>
2025-06-21 14:15:23 +0530

    initial commit

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -30,7 +30,7 @@ export default function FooterComp() {
                       target='_blank'
                       rel='noopener noreferrer'
                     >
-                        Rishabh's Blogify
+                        Blogify
                     </Footer.Link>
                    </Footer.LinkGroup>
                 </div>
@@ -38,14 +38,14 @@ export default function FooterComp() {
                    <Footer.Title title='Follow us' />
                    <Footer.LinkGroup col>
                     <Footer.Link
-                       href='https://github.com/RishabhJain2404'
+                       href='https://github.com/Pranavsai058'
                        target='_blank'
                        rel='noopener noreferrer'
                     >
                         GitHub
                     </Footer.Link>
                     <Footer.Link
-                       href='https://www.linkedin.com/in/rishabhjain2404/'
+                       href='https://www.linkedin.com/in/pranav-sai-kapilavai-510626252/'
                        target='_blank'
                        rel='noopener noreferrer'
                     >
@@ -66,15 +66,15 @@ export default function FooterComp() {
         <div className="w-full sm:flex sm:items-center sm:justify-between">
           <Footer.Copyright 
             href="#"
-            by="Rishabh's blogify"
+            by="blogify"
             year={new Date().getFullYear()}
           />
           <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
             <Footer.Icon href='#' icon={BsFacebook}/>
             <Footer.Icon href='#' icon={BsInstagram}/>
             <Footer.Icon href='#' icon={BsTwitter}/>
-            <Footer.Icon href='https://github.com/RishabhJain2404' icon={BsGithub}/>
-            <Footer.Icon href='https://www.linkedin.com/in/rishabhjain2404/' icon={BsLinkedin}/>
+            <Footer.Icon href='https://github.com/Pranavsai058' icon={BsGithub}/>
+            <Footer.Icon href='https://www.linkedin.com/in/pranav-sai-kapilavai-510626252/' icon={BsLinkedin}/>
           </div>
         </div>
       </div>
